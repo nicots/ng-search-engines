94b88825a4cd149a59f2ed21d54eb308871aa19a
prokopis3 <prokopis123@gmail.com>
2017-12-02 01:08:04 +0200

    app header navbar updated

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  styleUrls: ['./app-header.component.scss'],
   templateUrl: './app-header.component.html'
 })
 export class AppHeaderComponent { }
